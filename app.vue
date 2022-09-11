d2ac0495208218c1cbb6da5eb9cfff55ac0f0c48
Stephen Hebert <stephen@goteamup.com>
2022-09-11 17:27:29 -0500

    add auth middleware

--- app.vue
+++ app.vue
@@ -4,3 +4,16 @@
     <NuxtPage />
   </div>
 </template>
+
+<script>
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBlKOFUyYepKkE_982rIjq4umOauj4avtY",
+  authDomain: "dev-checklist-c2225.firebaseapp.com",
+  projectId: "dev-checklist-c2225",
+  storageBucket: "dev-checklist-c2225.appspot.com",
+  messagingSenderId: "140024092391",
+  appId: "1:140024092391:web:263055ab45f66d39ec7442"
+};
+export const firebaseApp = initializeApp(firebaseConfig);
+</script>
