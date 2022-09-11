be5a28b0d00a47bf9cfd0b50d25b90aec019318d
Stephen Hebert <stephen@goteamup.com>
2022-09-11 17:31:55 -0500

    auth with redirect

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,4 +1,4 @@
-import { getAuth, signInWithPopup, GithubAuthProvider } from "firebase/auth";
+import { getAuth, signInWithRedirect, GithubAuthProvider } from "firebase/auth";
 
 export default function (context) {
 
@@ -9,27 +9,28 @@ export default function (context) {
 
 
   const auth = getAuth();
-  
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-      const credential = GithubAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
+  signInWithRedirect(auth, provider);
 
-      // The signed-in user info.
-      const user = result.user;
-      console.log('user', user)
-      // ...
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GithubAuthProvider.credentialFromError(error);
-      // ...
-    });
+  // signInWithPopup(auth, provider)
+  //   .then((result) => {
+  //     // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+  //     const credential = GithubAuthProvider.credentialFromResult(result);
+  //     const token = credential.accessToken;
+
+  //     // The signed-in user info.
+  //     const user = result.user;
+  //     console.log('user', user)
+  //     // ...
+  //   }).catch((error) => {
+  //     // Handle Errors here.
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+  //     // The email of the user's account used.
+  //     const email = error.customData.email;
+  //     // The AuthCredential type that was used.
+  //     const credential = GithubAuthProvider.credentialFromError(error);
+  //     // ...
+  //   });
 
 
   
